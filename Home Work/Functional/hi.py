a4c59000f543b86113649712e68e61dbed05e693
alexnegrya <82027665+alexnegrya@users.noreply.github.com>
2021-04-10 13:05:10 +0300

    added new direction

---
+++ Home Work/Functional/hi.py
@@ -0,0 +1,16 @@
+# function definition
+def hi (lang, name):
+    if lang == 'en':
+        print(f'Hello, {name}!')
+    elif lang == 'ru':
+        print(f'Привет, {name}!')
+    elif lang == 'ro':
+        print(f'Salut, {name}!')
+    else:
+        print(f'Error: language [{lang}] is not recongrized!')
+
+# function call
+hi('en', 'Sarah')
+hi('ru', 'Иван')
+hi('ro', 'Ion')
+hi('ge', 'Kolya')
---
+++ Home Work/Functional/math.py
@@ -0,0 +1,18 @@
+def mul(a, b):
+    r = a * b
+    return r
+
+def div(a, b):
+    r = a / b
+    return r
+
+def add(a, b):
+    r = a + b
+    return r
+
+def sub(a, b):
+    r = a + b
+    return r
+
+result = div( mul( add(1, 2), 3 ), 4 )
+print(result)
