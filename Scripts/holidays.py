decff210423a046fd38a67282b55e8653884ea16
alexnegrya <82027665+alexnegrya@users.noreply.github.com>
2021-08-17 16:01:57 +0300

    added script for getting holidays by the country

---
+++ Scripts/holidays.py
@@ -0,0 +1,54 @@
+import requests
+from os import system
+
+# Get user ip address, then country code
+def getCode():
+    url = 'https://freegeoip.app/json/'
+    res = requests.get(url)
+    data = res.json()
+    c = data['country_code']
+    return c
+
+def wait():
+    input('\nContinue... ')
+
+def clr():
+    system('clear')
+
+while True:
+    print(
+        '1) Print all holidays\
+        \n2) Search holidays by name or letter(s)\
+        \n3) Exit'
+    )
+    option = input('\n>>> ')
+
+    if option == '1' or option == '2':
+        year = input('\nYear: ')
+
+        print('Waiting for a server responce... ')
+        res = requests.get(f"https://date.nager.at/api/v2/PublicHolidays/{year}/{getCode()}")
+        data = res.json()
+        clr()
+
+    if option == '1':
+        for i in range(len(data)):
+            print(f"{i + 1}) {data[i]['name']}")
+    elif option == '2':
+        search = input('Enter name or letter(s): ')
+        for i in range(len(data)):
+            result = []
+            if search in data[i]['name']:
+                result.append(data[i]['name'])
+                print(f"{i + 1}) {data[i]['name']}")
+                if len(result) == 0:
+                    print('Nothing found')
+    elif option == '3':
+        clr()
+        print('Thanks you for using my app!')
+        break
+    else:
+        print('Choose correct option!')
+    wait()
+    clr()
+
